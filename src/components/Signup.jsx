c07bed3a13ab68837cc575cb29de02381bae2089
Julia Löw <julia.loew@gmx.net>
2024-08-27 13:30:49 +0200

    Behebung Konflikt

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -80,11 +80,11 @@ export default function SignupForm() {
                     className="grow" placeholder="email" />
                 </label>
 
-                {/* <label className="input input-bordered flex items-center gap-2 w-full">
+                <label className="input input-bordered flex items-center gap-2 w-full">
                   User Name
                   <input onChange={handleChange} value={signupData.username} type="text"
                     name="username" className="grow" placeholder="username" />
-                </label> */}
+                </label>
 
                 <label className="input input-bordered flex items-center gap-2 w-full">
                   Passwort
