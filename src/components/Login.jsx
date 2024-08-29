35b5e2c27178061a7dfcddd3357ffd424de1c8ee
Sebastian Olthoff <sebustotules@gmail.com>
2024-08-29 12:07:16 +0200

    update user data added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,11 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "./AuthContext";
 import { Navigate } from "react-router-dom";
-import Modal from 'react-modal';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Modal from "react-modal";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 export default function Login() {
   const { login, userInfo } = useContext(AuthContext);
@@ -42,7 +42,9 @@ export default function Login() {
   }
 
   function handleForgotPasswordSubmit() {
-    toast.success("Eine E-Mail zur Passwort-Zurücksetzung wurde gesendet. Bitte überprüfen Sie Ihr Postfach.");
+    toast.success(
+      "Eine E-Mail zur Passwort-Zurücksetzung wurde gesendet. Bitte überprüfen Sie Ihr Postfach."
+    );
     closeModal();
   }
 
@@ -54,24 +56,51 @@ export default function Login() {
       ) : (
         <div className="max-w-2xl mx-auto flex flex-col gap-6 items-center py-32 mt-16 bg-teal-200 rounded-2xl">
           <label className="input input-bordered flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
               <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
-            <input name="email" onChange={handleChange} type="text" className="grow" placeholder="Email" value={loginData.email} />
+            <input
+              name="email"
+              onChange={handleChange}
+              type="text"
+              className="grow"
+              placeholder="Email"
+              value={loginData.email}
+            />
           </label>
 
           <label className="input input-bordered flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
               <path
                 fillRule="evenodd"
                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                 clipRule="evenodd"
               />
             </svg>
-            <input name="password" onChange={handleChange} type="password" className="grow" value={loginData.password} placeholder="Passwort" />
+            <input
+              name="password"
+              onChange={handleChange}
+              type="password"
+              className="grow"
+              value={loginData.password}
+              placeholder="Passwort"
+            />
           </label>
-          <button onClick={handleLogin} className="btn bg-yellow-400 border-black px-8 font-normal">
+          <button
+            onClick={handleLogin}
+            className="btn bg-yellow-400 border-black px-8 font-normal"
+          >
             Login
           </button>
 
@@ -90,8 +119,16 @@ export default function Login() {
           </a>
 
           {error && (
-            <div role="alert" className="alert alert-warning flex items-center gap-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+            <div
+              role="alert"
+              className="alert alert-warning flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -108,7 +145,7 @@ export default function Login() {
             onRequestClose={closeModal}
             contentLabel="Passwort vergessen"
             className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg"
-            overlayClassName="fixed inset-0 bg-black bg-opacity-75"
+            overlayClassName="bg-[rgba(59,130,246,0.5)] fixed inset-0"
             shouldCloseOnOverlayClick={true}
             shouldCloseOnEsc={true}
             ariaHideApp={false}
@@ -124,7 +161,10 @@ export default function Login() {
                 value={forgotPasswordEmail}
               />
             </label>
-            <button onClick={handleForgotPasswordSubmit} className="btn bg-yellow-400 border-black px-8 font-normal">
+            <button
+              onClick={handleForgotPasswordSubmit}
+              className="btn bg-yellow-400 border-black px-8 font-normal"
+            >
               Passwort zurück setzen
             </button>
           </Modal>
